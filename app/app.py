4fef147a8432ac4dfc1c7cfb3e582b58d5320fca
hardurathod <hardurathod11@gmail.com>
2025-11-05 17:24:48 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask Docker!"
+    return "Hello from hardik !"
 
 if __name__ == '__main__':
     # Important: host='0.0.0.0' lets it accept requests from outside Docker
